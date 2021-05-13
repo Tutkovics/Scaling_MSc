a47b0854100edf7c510d388a46af4cd6f7b58753
Tutkovics <tutkovics@sch.bme.hu>
2021-05-13 21:27:21 +0200

    Extend thesis

---
+++ thesis/figures/kubectl_get_nodes.sh
@@ -0,0 +1,5 @@
+$ kubectl get nodes
+NAME       STATUS   ROLES                  AGE   VERSION
+dipterv1   Ready    control-plane,master   30d   v1.21.0
+dipterv2   Ready    <none>                 30d   v1.21.0
+dipterv3   Ready    <none>                 30d   v1.21.0
