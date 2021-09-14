29b32d57cd87b2b6d90c353e52cfa46259e66f25
Tutkovics <tutkovics@sch.bme.hu>
2021-09-14 21:27:41 +0200

    Add test algorithm

---
+++ tight-loop/testal.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Starting...")
+
+	algorithm(3000)
+
+	fmt.Println("Bye my Friend!")
+}
+
+func algorithm(milliseconds int) {
+	now := time.Now() //.Nanosecond() / 1000000
+
+	duration := milliseconds * int(time.Millisecond)
+	iteration := 0
+
+	fmt.Println(time.Now())
+
+	for now.Add(time.Duration(duration)).After(time.Now()) {
+		iteration += 1
+	}
+
+	fmt.Println("# of runned iteration >", iteration, "<")
+
+	fmt.Println(time.Now())
+
+}
