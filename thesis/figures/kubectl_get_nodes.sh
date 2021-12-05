bfc6f7414a1c6b419ad148cb9e4b6b8dcdd44251
Tutkovics <tutkovics@sch.bme.hu>
2021-12-05 21:39:44 +0100

    Fix typos + add 12pt font and margins.

--- thesis/figures/kubectl_get_nodes.sh
+++ thesis/figures/kubectl_get_nodes.sh
@@ -1,5 +1,5 @@
 $ kubectl get nodes
 NAME       STATUS   ROLES                  AGE   VERSION
-dipterv1   Ready    control-plane,master   30d   v1.21.0
-dipterv2   Ready    <none>                 30d   v1.21.0
-dipterv3   Ready    <none>                 30d   v1.21.0
+dipterv1   Ready    control-plane,master   236d   v1.21.0
+dipterv2   Ready    <none>                 236d   v1.21.0
+dipterv3   Ready    <none>                 236d   v1.21.0
