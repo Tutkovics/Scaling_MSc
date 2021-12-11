3f4ea12bf7436bfbb3072928b883c066d74e11ea
Tutkovics <tutkovics@sch.bme.hu>
2021-12-11 22:46:41 +0100

    LeanNet review version

--- thesis/figures/hpa_example.sh
+++ thesis/figures/hpa_example.sh
@@ -15,9 +15,9 @@ spec:
 $ while sleep 0.01; do wget -q -O- http://localhost:30100; done
 
 $ kubectl get horizontalpodautoscalers                                               
-NAME         REFERENCE               TARGETS    MINPODS   MAXPODS   REPLICAS   AGE                        
-php-apache   Deployment/php-apache   179%/50%   1         10        1          4m53s                      
+NAME        REFERENCE              TARGETS   MINPODS  MAXPODS  REPLICAS  AGE                        
+php-apache  Deployment/php-apache  179%/50%  1        10       1         4m53s                      
 ...
-php-apache   Deployment/php-apache   81%/50%    1         10        4          6m35s                      
+php-apache  Deployment/php-apache  81%/50%   1        10       4         6m35s                      
 ...
-php-apache   Deployment/php-apache   42%/50%    1         10        7          10m  
+php-apache  Deployment/php-apache  42%/50%   1        10       7         10m  
