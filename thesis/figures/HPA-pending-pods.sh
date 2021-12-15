1c98654505e9d242b00fa3cfcc2b4afd367df3c2
Tutkovics <tutkovics@sch.bme.hu>
2021-12-15 21:19:19 +0100

    Change small measurement graphicons to subfigures

--- thesis/figures/HPA-pending-pods.sh
+++ thesis/figures/HPA-pending-pods.sh
@@ -1,7 +1,7 @@
 $ kubectl get horizontalpodautoscalers
-NAME          REFERENCE                TARGETS    MINPODS   MAXPODS   REPLICAS   AGE
-back-end-1    Deployment/back-end-1    100%/70%   1         5         5          5m34s
-front-end-1   Deployment/front-end-1   80%/70%    1         5         4          5m34s
+NAME         REFERENCE               TARGETS   MINPODS MAXPODS REPLICAS AGE
+back-end-1   Deployment/back-end-1   100%/70%  1       5       5        5m34s
+front-end-1  Deployment/front-end-1  80%/70%   1       5       4        5m34s
 
 $ kubectl get all
 NAME                               READY   STATUS    RESTARTS   AGE
@@ -14,4 +14,3 @@ pod/front-end-1-6677c56849-26nmn   1/1     Running   0          5m34s
 pod/front-end-1-6677c56849-26zr2   1/1     Running   0          3m48s
 pod/front-end-1-6677c56849-8s9ph   0/1     Pending   0          48s
 pod/front-end-1-6677c56849-fndnw   1/1     Running   0          2m48s
-...
